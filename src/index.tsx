6eb9ac1d296f5ede03ee26ce394a9e243bbc264d
unknown <arinsuga@gmail.com>
2023-11-13 14:01:17 +0700

    dev1.0 - tambah file style.css untuk sample

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
+import './assets/css/styles.css';
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
