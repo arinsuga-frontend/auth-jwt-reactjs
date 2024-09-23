ae30c6841d0ffab30d716acf335f94b4fe098790
sugaprivate <arinsuga@gmail.com>
2024-09-23 13:07:40 +0700

    reduxjs - update

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import {Dropdownsearch} from 'arsuga-reactlib-dropdownsearch';
+
 export default function Todo()
 {
 
     return (
         <>
             <h1>TODO List</h1>
+            <Dropdownsearch placeholder="Input disini" searchable="true" />
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi deserunt hic commodi expedita, in debitis quisquam dolorum nihil excepturi ut similique laudantium ducimus. Vitae, commodi est! Id error quibusdam natus?</p>
         </>
     )
